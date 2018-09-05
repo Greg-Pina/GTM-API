f33093e7d31be23cb4ab079f1c298e786d9ec18d
Greg Pina <greg.pina@infotrustllc.com>
2018-09-05 13:37:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# GregPina-APITestRepo
---
+++ src/main/java/GTM_APITest.java
@@ -0,0 +1,126 @@
+
+
+	/**
+	 * Access and manage a Google Tag Manager account.
+	 */
+
+	import java.io.File;
+	import java.io.InputStreamReader;
+	import java.util.ArrayList;
+	import java.util.Arrays;
+	import java.util.List;
+
+	import org.apache.commons.codec.language.bm.Rule;
+
+	import com.google.api.client.auth.oauth2.Credential;
+	import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+	import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+	import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+	import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+	import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+	import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+	import com.google.api.client.http.javanet.NetHttpTransport;
+	import com.google.api.client.json.JsonFactory;
+	import com.google.api.client.json.gson.GsonFactory;
+	import com.google.api.client.util.store.FileDataStoreFactory;
+	import com.google.api.services.tagmanager.TagManager;
+	import com.google.api.services.tagmanager.TagManagerScopes;
+	import com.google.api.services.tagmanager.model.Condition;
+	import com.google.api.services.tagmanager.model.Container;
+	import com.google.api.services.tagmanager.model.Parameter;
+	import com.google.api.services.tagmanager.model.Tag;
+	public class GTM_APITest {
+		
+		  // Path to client_secrets.json file downloaded from the Developer's Console.
+		  // The path is relative to GoogleTagManager.java.
+		  private static final String CLIENT_SECRET_JSON_RESOURCE = "client_secrets.json";
+
+		  // The directory where the user's credentials will be stored for the application.
+		  private static final File DATA_STORE_DIR = new File("/Users/Greg/eclipse-workspace/Google_Tag_Manager/src/main/java/client_secrets.json\n" + 
+		  		"\n" + 
+		  		"");
+
+		  private static final String Google_Tag_Manager = "GoogleTagManager";
+		  private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+		  private static NetHttpTransport httpTransport;
+		  private static FileDataStoreFactory dataStoreFactory;
+
+		  public static void main(String[] args) {
+		    try {
+		    	
+
+		      httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+		      dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
+
+		      // Authorization flow.
+		      Credential credential = authorize();
+		      TagManager manager = new TagManager.Builder(httpTransport, JSON_FACTORY, credential)
+		          .setApplicationName(Google_Tag_Manager).build();
+
+		      // Get tag manager account ID.
+		      String accountId = "3982950028";
+
+		      // Find the Greg_Pina_Test container.
+		      Container Greg_Pina_Test = findContainer(manager, accountId);
+		      String containerId = Greg_Pina_Test.getContainerId();
+		      
+		      /*
+		       * Return example container (as object)
+		       * Return all tags from example container as arraylist
+		       * Return all triggers from example container as arraylist
+		       * Return all variables from example container as arraylist
+		       * 
+		       * For Each var in VarExample
+		       * 	Create variable in Greg_Pina test Container
+		       * 
+		       *    used object as return as param
+		       *    
+		       *    Then use .ToList method (or w/e) 
+		       *    
+		       *    
+		       *    
+		       */
+
+		    } catch (Exception e) {
+		      e.printStackTrace();
+		    }
+		  }
+
+		private static Credential authorize() throws Exception {
+		    // Load client secrets.
+		    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+		        new InputStreamReader(GoogleTagManager.class.getResourceAsStream(CLIENT_SECRET_JSON_RESOURCE)));
+
+		    // Set up authorization code flow for all auth scopes.
+		    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport,
+		        JSON_FACTORY, clientSecrets, TagManagerScopes.all()).setDataStoreFactory(dataStoreFactory)
+		        .build();
+
+		    // Authorize.
+		    return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+		  }
+
+		  /*
+		   * Find the Greg_Pina_Test container ID.
+		   *
+		   * @param service the Tag Manager service object.
+		   * @param accountId the ID of the Tag Manager account from which to retrieve the
+		   *    Greetings container.
+		   *
+		   * @return the Greg_Pina_Test container if it exists.
+		   *
+		   */
+		  private static Container findContainer(TagManager service, String accountId)
+		      throws Exception {
+		    for (Container container :
+		        service.accounts().containers().list(accountId).execute().getContainer()) {
+		      if (container.getName().equals("Greg_Pina")) {
+		        return container;
+		      }
+		    }
+		    throw new IllegalArgumentException("No container named Greg_Pina in given account");
+		  }
+		}
+		      
+
+}
